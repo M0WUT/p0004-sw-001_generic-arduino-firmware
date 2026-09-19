7a93043aafab3fdafed1191aee793f14dc46be0e
M0WUT <dan.m0wut@gmail.com>
2026-09-19 22:35:38 +0100

    wip

--- lib/ServiceId/ServiceId.h
+++ lib/ServiceId/ServiceId.h
@@ -1,6 +1,3 @@
-#ifndef SERVICEID_H
-#define SERVICEID_H
-
 #include "enum.h"
 
 // Definitions here to exact numbers are important and must not be changed
@@ -8,5 +5,3 @@
 // This Enum is expected to be bounded to fit in a uint8_t elsewhere -if I've written more than
 // 256 services, this has gone much better (or worse!) than planned
 BETTER_ENUM(ServiceId, uint8_t, CORE = 0, POWER, THERMAL, LCD, ETHERNET, USB_HOST, USB_DEVICE, SD_CARD, UI, SCPI, HOME_ASSISTANT, APPLICATION)
-
-#endif // SERVICEID_H
